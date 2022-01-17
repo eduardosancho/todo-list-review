e8cb1782e639fc542fbee6299ff5e99573501e69
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-17 15:30:23 -0600

    copy files from repo to-do-list

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+import ToDoList from './modules/ToDoList.js';
+
+const toDoList = new ToDoList();
+
+const addTaskForm = document.getElementById('add-task-form');
+addTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  toDoList.addTask();
+});
+
+const addTaskButton = document.getElementById('add-task');
+addTaskButton.addEventListener('click', () => toDoList.addTask());
+
+const removeCompletedBtn = document.getElementById('clear-completed');
+removeCompletedBtn.addEventListener('click', () => {
+  toDoList.removeCompleted();
+});
+
+toDoList.displayList();
